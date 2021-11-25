a3675a6fcc040674b7d483a5cc6c71fefbaba0ee
Scalable Architecture Lab <ScalableArchiLab@taurus.(none)>
2021-11-25 12:27:31 +0900

    The name of RR-policy was .v3.fixed_registered.sv => fixed

--- lab10/DMAC/RTL/DMAC_ARBITER.v3.fixed_registered.sv
+++ lab10/DMAC/RTL/DMAC_ARBITER.v3.fixed_registered.sv
@@ -22,74 +22,64 @@ module DMAC_ARBITER
     input   wire                dst_ready_i,
     output  reg     [DATA_SIZE-1:0] dst_data_o
 );
-    localparam N_MASTER_LG2     = $clog2(N_MASTER);
 
-    reg     [N_MASTER_LG2-1:0]  rotate_ptr;
-    reg                         shift_valid[N_MASTER];
-    reg     [DATA_SIZE-1:0]     shift_data[N_MASTER];
-    reg                         shift_ready[N_MASTER];
+    enum reg {S_IDLE, S_BUSY}   state,    state_n;
+    reg                         dst_valid,  dst_valid_n;
+    reg     [DATA_SIZE-1:0]     dst_data,   dst_data_n;
 
     always_ff @(posedge clk)
         if (~rst_n) begin
-            rotate_ptr          <= 'd0;
+            state                   <= S_IDLE;
+            dst_valid               <= 1'b0;
+            dst_data                <= 'd0;
         end
         else begin
-            if (src_valid_i[0] & src_ready_o[0]) begin
-                rotate_ptr          <= 2'd1;
-            end
-            else if (src_valid_i[1] & src_ready_o[1]) begin
-                rotate_ptr          <= 2'd2;
-            end
-            else if (src_valid_i[2] & src_ready_o[2]) begin
-                rotate_ptr          <= 2'd3;
-            end
-            else if (src_valid_i[3] & src_ready_o[3]) begin
-                rotate_ptr          <= 2'd0;
-            end
+            state                   <= state_n;
+            dst_valid               <= dst_valid_n;
+            dst_data                <= dst_data_n;
         end
-
-    always_comb begin
-        case (rotate_ptr)
-            'd0: begin
-                shift_valid         = {src_valid_i[3], src_valid_i[2], src_valid_i[1], src_valid_i[0]};
-                shift_data          = {src_data_i[3], src_data_i[2], src_data_i[1], src_data_i[0]};
-                src_ready_o         = {shift_ready[3], shift_ready[2], shift_ready[1], shift_ready[0]};
-            end
-            'd1: begin
-                shift_valid         = {src_valid_i[0], src_valid_i[3], src_valid_i[2], src_valid_i[1]};
-                shift_data          = {src_data_i[0], src_data_i[3], src_data_i[2], src_data_i[1]};
-                src_ready_o         = {shift_ready[0], shift_ready[3], shift_ready[2], shift_ready[1]};
-            end
-            'd2: begin
-                shift_valid         = {src_valid_i[1], src_valid_i[0], src_valid_i[3], src_valid_i[2]};
-                shift_data          = {src_data_i[1], src_data_i[0], src_data_i[3], src_data_i[2]};
-                src_ready_o         = {shift_ready[1], shift_ready[0], shift_ready[3], shift_ready[2]};
-            end
-            'd3: begin
-                shift_valid         = {src_valid_i[2], src_valid_i[1], src_valid_i[0], src_valid_i[3]};
-                shift_data          = {src_data_i[2], src_data_i[1], src_data_i[0], src_data_i[3]};
-                src_ready_o         = {shift_ready[2], shift_ready[1], shift_ready[0], shift_ready[3]};
-            end
-        endcase
-    end
 
     // fixed priority arbiter
     always_comb begin
         // default
-        dst_valid_o             = 1'b0;
-        dst_data_o              = shift_data[3];    // don't care
+        state_n                 = state;
+        dst_valid_n             = dst_valid;
+        dst_data_n              = dst_data;
         for (int i=0; i<N_MASTER; i++) begin
-            shift_ready[i]          = 1'b0;
+            src_ready_o[i]          = 1'b0;
         end
 
-        for (int i=0; i<N_MASTER; i++) begin
-            if (shift_valid[i]) begin
-                dst_valid_o             = 1'b1;
-                dst_data_o              = shift_data[i];
-                shift_ready[i]          = dst_ready_i;
-                break;
+        // there's no valid request
+        if (state==S_IDLE) begin
+            for (int i=0; i<N_MASTER; i++) begin
+                if (src_valid_i[i]) begin
+                    state_n                 = S_BUSY;
+                    dst_valid_n             = 1'b1;
+                    dst_data_n              = src_data_i[i];
+                    src_ready_o[i]          = 1'b1;
+                    break;
+                end
+            end
+        end
+        else // state==I_BUSY
+        begin
+            if (dst_ready_i) begin
+                state_n                 = S_IDLE;
+                dst_valid_n             = 1'b0;
+                for (int i=0; i<N_MASTER; i++) begin
+                    if (src_valid_i[i]) begin
+                        dst_valid_n             = 1'b1;
+                        dst_data_n              = src_data_i[i];
+                        src_ready_o[i]          = 1'b1;
+                        state_n                 = S_BUSY;
+                        break;
+                    end
+                end
             end
         end
     end
+
+    assign  dst_valid_o             = dst_valid;
+    assign  dst_data_o              = dst_data;
 
 endmodule
